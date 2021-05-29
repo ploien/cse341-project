ebda3255e43b32a65418376527ca0615656b1f5b
ploien <oie17001@byui.edu>
2021-05-28 22:28:43 -0700

    Removed space from json file name

--- models/project01/product.js
+++ models/project01/product.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const rootDir = require('../../util/path');
-const p = path.join(rootDir, 'data', 'products.json ');
+const p = path.join(rootDir, 'data', 'products.json');
 
 module.exports = class Product {
     constructor(productName, itemDescription, price) {
